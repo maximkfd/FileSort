7c4688c15ea654dd8b3ab72bdab52c83e82ac3eb
Denis Repp <denis.repp@team.wrike.com>
2017-03-04 15:16:21 +0300

    refactored

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
-@Deprecated
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
+        //uncomment ot generate file of 1 gb
+//        Generator generator = new Generator();
+//        generator.generate("input.txt", 500);
 
+        final Sorter sorter = new Sorter();
+        sorter.sort("input.txt");
 
     }
+
 }
--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -13,47 +13,47 @@ import java.util.ArrayList;
 public class Sorter {
 
     public void sort(final String filename) throws IOException {
-        final Runtime runtime = Runtime.getRuntime();
-        final long begin = System.currentTimeMillis();
+        Runtime runtime = Runtime.getRuntime();
+        long begin = System.currentTimeMillis();
 
         System.out.println(Runtime.getRuntime().totalMemory() + ":  " + Runtime.getRuntime().freeMemory());
-        final BufferedReader reader = new BufferedReader(new FileReader(filename));
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
         //прочитать несколько строк
         ArrayList<String> list;
 
-        final File sortedFileIn = File.createTempFile("to read", null);
-        final File sortedFile = File.createTempFile("to write", null);
-        final File result = new File("res");
-        // fix 1
-        if (!result.exists()) {
-            result.createNewFile();
-        }
-        // fix1 end
+        File sortedFileIn = File.createTempFile("to read", null);
+        File sortedFile = File.createTempFile("to write", null);
+        File result = new File("res");
         int k = 0;
         while (reader.ready()) {
-            System.gc();
-
-            final long size = 0;
-            //Читаем кусок из 1gb файла
+//            System.gc();
             list = new ArrayList<>();
-            while (reader.ready() &&  runtime.freeMemory() > runtime.totalMemory() - runtime.freeMemory()) {
-                list.add(reader.readLine());
+
+            long size = 0;
+            //Читаем кусок из 1gb файла
+//            while (reader.ready() &&  runtime.freeMemory() > runtime.totalMemory() - runtime.freeMemory()) {
+            while (reader.ready() &&  size < 32000000) {
+                final String tmp = reader.readLine();
+                size += tmp.getBytes().length;
+                list.add(tmp);
+//                list.add(reader.readLine());
             }
             list.sort(null);
-
+            System.out.println("read " + size + " bytes");
             if (sortedFileIn.length() == 0) {
                 //Создаём временный файл из первой партии строк
-                final BufferedWriter out = new BufferedWriter(new FileWriter(sortedFileIn));
-                for (final String s :
+                BufferedWriter out = new BufferedWriter(new FileWriter(sortedFileIn));
+                for (String s :
                         list) {
-                    out.write(s + "\r\n");
+                    out.write(s);
+                    out.newLine();
                 }
 //                out.writeObject("Holy fuck");
                 out.close();
             } else {
                 //Сливаем с отсортированным временным файлом в другой временный файл
-                final BufferedReader in = new BufferedReader(new FileReader(sortedFileIn));
-                final BufferedWriter out = new BufferedWriter(new FileWriter(sortedFile));
+                BufferedReader in = new BufferedReader(new FileReader(sortedFileIn));
+                BufferedWriter out = new BufferedWriter(new FileWriter(sortedFile));
                 String tmp = null;
                 int i = 0;
                 tmp = in.readLine();
@@ -79,8 +79,8 @@ public class Sorter {
                 out.close();
                 in.close();
                 //меняем файлы местами
-                final boolean delete = sortedFileIn.delete();
-                final boolean rename = sortedFile.renameTo(sortedFileIn);
+                boolean delete = sortedFileIn.delete();
+                boolean rename = sortedFile.renameTo(sortedFileIn);
 
             }
             k += list.size();
@@ -88,13 +88,13 @@ public class Sorter {
         //перемещаем временный файл в ответ
         try {
             Files.delete(result.toPath());
-        } catch (final IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         Files.move(sortedFileIn.toPath(), result.toPath());
         try {
             sortedFileIn.delete();
-        } catch (final Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
